c48455a3ff927c81a8ad8ba9d9f04b9a01526973
AdamDzierzko <adzierzko7@gmail.com>
2018-09-07 08:19:16 +0200

    1

---
+++ src/main/java/adam/magazyn/MagazynApplication.java
@@ -0,0 +1,12 @@
+package adam.magazyn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagazynApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MagazynApplication.class, args);
+    }
+}
